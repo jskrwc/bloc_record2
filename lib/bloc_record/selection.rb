fb17785642c76dc883bf2a908c43ff2bc33deafd
Jim Knopf <jsk4@mac.com>
2019-05-22 23:49:43 -0700

    Revert "assignment 3 completed"
    This reverts commit 069276cbf6576afec96d700ef01b631c74c8111f.

--- lib/bloc_record/selection.rb
+++ lib/bloc_record/selection.rb
@@ -3,10 +3,9 @@ require 'sqlite3'
 module Selection
 
   def find(*ids)
+
     if ids.length == 1
       find_one(ids.first)
-    elsif ids.id_input_error?       # validate the ids - error found?
-      raise ArgumentError.new('Ids must be positive integers!')
     else
       rows = connection.execute <<-SQL
         SELECT #{columns.join ","} FROM #{table}
@@ -18,17 +17,12 @@ module Selection
   end
 
   def find_one(id)
-    if Array(id).id_input_error?
-       # private method to check for pos integer input - evaluates arrays
-      raise ArgumentError.new('Id must be a positive integer!')
-    else
-      row = connection.get_first_row <<-SQL
-        SELECT #{columns.join ","} FROM #{table}
-        WHERE id = #{id};
-      SQL
+    row = connection.get_first_row <<-SQL
+      SELECT #{columns.join ","} FROM #{table}
+      WHERE id = #{id};
+    SQL
 
-      init_object_from_row(row)
-    end
+    init_object_from_row(row)
   end
 
   def find_by(attribute, value)
@@ -39,35 +33,6 @@ module Selection
 
     rows_to_array(rows)
   end
-
-  def method_missing(m, *args, &block)
-    # if m == :find_by_name
-    #   find_by(:name, +args[0])    # *****
-    if m.to_s = ~/find_by_(.*)/
-      find_by(   ,+args[0])    # *****
-    end
-  end
-
-  def find_each(opts = {})  # expect start and batch size, block
-    rows = connection.execute <<-SQL
-      SELECT #{columns.join ","} FROM #{table}
-      LIMIT #{opts[:batch_size]};
-    SQL
-
-    for row in rows_to_array(rows)
-      yield(row)
-    end
-  end
-
-  def find_in_batches(opts = {})
-    rows = connection.execute <<-SQL
-      SELECT #{columns.join ","} FROM #{table}
-      LIMIT #{opts[:batch_size]};
-    SQL
-
-    yield(rows_to_array(rows))
-  end
-
 
   def take(num=1)
     if num > 1
@@ -122,25 +87,14 @@ end
 
   private
 
-  def init_object_from_row(row)  # converts row into an object
+  def init_object_from_row(row)
     if row
       data = Hash[columns.zip(row)]
       new(data)
     end
   end
 
-  def rows_to_array(rows)  # gets records, converts the objs into array
+  def rows_to_array(rows)
     rows.map { |row| new(Hash[columns.zip(row)]) }
   end
-
-  def id_input_error(ids)  # exception if not positive integer
-    ids.each do |n|
-      if n < 0 || !n.is_a? Integer
-        return true   # exception found
-      end
-    end
-    return false
-  end
-
-
 end
