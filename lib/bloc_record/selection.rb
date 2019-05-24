dddb2f331af401ca1f5a77e3016e9d2c241f8adf
Jim Knopf <jsk4@mac.com>
2019-05-23 22:00:59 -0700

    checkpoint 4 completed

--- lib/bloc_record/selection.rb
+++ lib/bloc_record/selection.rb
@@ -34,6 +34,36 @@ module Selection
     rows_to_array(rows)
   end
 
+  def method_missing(m, *args, &block)
+    # if m == :find_by_name
+    #   find_by(:name, +args[0])    # *****
+    #    if m.to_s = ~/find_by_(.*)/   # regex to split
+    if m.to_s[0..7] == "find_by_"      # split method into 'find_by_' + remainder
+      find_by(m.to_s[8..-1] ,+args[0])
+    end
+  end
+
+  def find_each(opts = {})  # expect start and batch size, block
+    rows = connection.execute <<-SQL
+      SELECT #{columns.join ","} FROM #{table}
+      LIMIT #{opts[:batch_size]};
+    SQL
+
+    for row in rows_to_array(rows)
+      yield(row)
+    end
+  end
+
+  def find_in_batches(opts = {})
+    rows = connection.execute <<-SQL
+      SELECT #{columns.join ","} FROM #{table}
+      LIMIT #{opts[:batch_size]};
+    SQL
+
+    yield(rows_to_array(rows))
+  end
+
+
   def take(num=1)
     if num > 1
       rows = connection.execute <<-SQL
@@ -77,12 +107,76 @@ module Selection
   end
 
   def all
-  rows = connection.execute <<-SQL
-    SELECT #{columns.join ","} FROM #{table};
-  SQL
+    rows = connection.execute <<-SQL
+      SELECT #{columns.join ","} FROM #{table};
+    SQL
 
-  rows_to_array(rows)
-end
+    rows_to_array(rows)
+  end
+
+
+  def where(*args)
+    if args.count > 1
+      expression = args.shift
+      params = args
+    else
+      case args.first
+      when String
+        expression = args.first
+      when Hash
+        expression_hash = BlocRecord::Utility.convert_keys(args.first)
+        expression = expression_hash.map {|key, value| "#{key}=#{BlocRecord::Utility.sql_strings(value)}"}.join(" and ")
+      end
+    end
+
+    sql = <<-SQL
+      SELECT #{columns.join ","} FROM #{table}
+      WHERE #{expression};
+    SQL
+
+    rows = connection.execute(sql, params)
+    rows_to_array(rows)
+  end
+
+  def order(*args)
+     if args.count > 1
+       order = args.join(",")
+     else
+       order = args.first.to_s
+     end
+
+    rows = connection.execute <<-SQL
+      SELECT * FROM #{table}
+      ORDER BY #{order};
+    SQL
+    rows_to_array(rows)
+  end
+
+
+  # def join(sql_string)   # eg. 'JOIN table_name ON some_condition'
+  def join(*args)
+    if args.count > 1
+      joins = args.map { |arg| "INNER JOIN #{arg} ON #{arg}.#{table}_id = #{table}.id"}.join(" ")
+      rows = connection.execute <<-SQL
+        SELECT * FROM #{table} #{joins}
+      SQL
+    else
+      case args.first
+      when String
+        rows = connection.execute <<-SQL
+          SELECT * FROM #{table} #{BlocRecord::Utility.sql_strings(args.first)};
+        SQL
+      when Symbol
+        rows = connection.execute <<-SQL
+          SELECT * FROM #{table}
+          INNER JOIN #{args.first} ON #{args.first}.#{table}_id = #{table}.id
+        SQL
+        # nb requires standard naming conventions to work  e.g. table_id column name
+      end
+    end
+
+    rows_to_array(rows)
+  end
 
 
   private
